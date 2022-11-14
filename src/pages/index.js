6c0ee63362fa94b0ac4ac64e914ec184fa3ec5b4
Qin Claes <qin.claes@student.ap.be>
2022-11-14 23:14:37 +0100

    adds image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Welcome to Inghelbrecht Agency!">
       <p>Lorem ipsum</p>
+      <StaticImage
+        alt="Just the two of us"
+        src="../images/QZL2FTi - Imgur.png"
+      />
     </Layout>
   )
 }
