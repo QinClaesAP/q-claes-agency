445dae51b4a5a13d8bc276ad9f5507c1b6862ab0
QinClaesAP <qin.claes@student.ap.be>
2022-12-01 19:43:01 +0100

    adds graphql query to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  console.log(data)
   return (
     <Layout pageTitle="Welcome to Q. Claes Agency!">
       <p>Lorem ipsum</p>
@@ -13,5 +15,50 @@ const IndexPage = () => {
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    wpPage(slug: { eq: "home" }) {
+      homeFields {
+        artists {
+          ... on WpArtist {
+            id
+            slug
+            artistMeta {
+              artistName
+              firstName
+              lastName
+              profilePicture {
+                altText
+                localFile {
+                  childImageSharp {
+                    gatsbyImageData(
+                      placeholder: BLURRED
+                      transformOptions: { grayscale: true }
+                    )
+                  }
+                }
+              }
+            }
+          }
+        }
+        callToAction {
+          title
+          url
+        }
+        title
+        description
+        picture {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED)
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
